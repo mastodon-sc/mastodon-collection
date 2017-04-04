079716f7d0b27963964d52994df3e11501ca5ab6
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-04-04 23:35:33 +0200

    Add TODO

--- src/main/java/org/mastodon/properties/HasPropertyMaps.java
+++ src/main/java/org/mastodon/properties/HasPropertyMaps.java
@@ -1,5 +1,17 @@
 package org.mastodon.properties;
 
+import org.mastodon.pool.Pool;
+
+
+// TODO: REMOVE?
+/**
+ * Something that has a {@link PropertyMaps} managing property maps. This is
+ * currently only implemented by {@link Pool}.
+ *
+ * @param <O>
+ *
+ * @author Tobias Pietzsch
+ */
 public interface HasPropertyMaps< O >
 {
 	public PropertyMaps< O > getPropertyMaps();
