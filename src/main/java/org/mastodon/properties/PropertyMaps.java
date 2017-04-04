60b11049072541bd5ff50190fa73540d5f87ee79
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-04-04 23:35:27 +0200

    javadoc

--- src/main/java/org/mastodon/properties/PropertyMaps.java
+++ src/main/java/org/mastodon/properties/PropertyMaps.java
@@ -14,6 +14,7 @@ import org.mastodon.RefPool;
  * <p>
  * The list only maintains {@link WeakReference}s to {@link PropertyMap}s. This
  * makes it easier to create temporary maps without worrying about book-keeping.
+ * Nevertheless, it is possible to explicitly remove maps.
  * </p>
  *
  * @param <O>
@@ -25,13 +26,21 @@ public class PropertyMaps< O >
 {
 	private final List< WeakReference< PropertyMap< O, ? > > > maps = new CopyOnWriteArrayList<>();
 
+	/**
+	 * Add a {@link PropertyMap}. The specified property map will be notified
+	 * about addition and removal of objects.
+	 *
+	 * @param map
+	 *            the property map to add.
+	 */
 	public void addPropertyMap( final PropertyMap< O, ? > map )
 	{
 		maps.add( new WeakReference< PropertyMap< O, ? > >( map ) );
 	}
 
 	/**
-	 * Explicitly remove a {@link PropertyMap}.
+	 * Remove a {@link PropertyMap}. The specified property map will no longer
+	 * be notified about addition and removal of objects.
 	 *
 	 * @param map
 	 */
@@ -40,6 +49,12 @@ public class PropertyMaps< O >
 		maps.removeIf( r -> map.equals( r.get() ) );
 	}
 
+	/**
+	 * Forward to {@link PropertyMap#remove(Object)} of all registered property
+	 * maps. Also cleans up maps that have been garbage collected.
+	 *
+	 * @param key
+	 */
 	public void remove( final O key )
 	{
 		boolean cleanUp = false;
@@ -55,6 +70,12 @@ public class PropertyMaps< O >
 			maps.removeIf( r -> null == r.get() );
 	}
 
+	/**
+	 * Forward to {@link PropertyMap#create(Object)} of all registered property
+	 * maps. Also cleans up maps that have been garbage collected.
+	 *
+	 * @param key
+	 */
 	public void create( final O key )
 	{
 		boolean cleanUp = false;
