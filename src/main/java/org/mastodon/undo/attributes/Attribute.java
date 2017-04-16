65246767e735a41212ccbdb88f1b67566b2653fe
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-04-16 02:15:14 +0200

    Let Attribute extend AbstractProperty

--- src/main/java/org/mastodon/undo/attributes/Attribute.java
+++ src/main/java/org/mastodon/undo/attributes/Attribute.java
@@ -1,18 +1,12 @@
 package org.mastodon.undo.attributes;
 
-import java.util.ArrayList;
+import org.mastodon.properties.AbstractProperty;
 
-public class Attribute< O >
+public class Attribute< O > extends AbstractProperty< O >
 {
 	private final AttributeSerializer< O > serializer;
 
 	private final String name;
-
-	private final ArrayList< BeforeAttributeChangeListener< O > > beforeChangeListeners;
-
-	private final ArrayList< AttributeChangeListener< O > > changeListeners;
-
-	private boolean emitEvents;
 
 	public Attribute( final AttributeSerializer< O > serializer )
 	{
@@ -23,9 +17,6 @@ public class Attribute< O >
 	{
 		this.serializer = serializer;
 		this.name = name;
-		beforeChangeListeners = new ArrayList<>();
-		changeListeners = new ArrayList<>();
-		emitEvents = true;
 	}
 
 	public AttributeSerializer< O > getUndoSerializer()
@@ -40,88 +31,6 @@ public class Attribute< O >
 	}
 
 	/**
-	 * Register a {@link BeforeAttributeChangeListener} that will be notified
-	 * before the value of this attribute is changed.
-	 *
-	 * @param listener
-	 *            the listener to register.
-	 * @return {@code true} if the listener was successfully registered.
-	 *         {@code false} if it was already registered.
-	 */
-	public boolean addBeforeAttributeChangeListener( final BeforeAttributeChangeListener< O > listener )
-	{
-		if ( !beforeChangeListeners.contains( listener ) )
-		{
-			beforeChangeListeners.add( listener );
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Removes the specified {@link BeforeAttributeChangeListener} from the set
-	 * of listeners.
-	 *
-	 * @param listener
-	 *            the listener to remove.
-	 * @return {@code true} if the listener was present in the listeners of this
-	 *         model and was successfully removed.
-	 */
-	public boolean removeBeforeAttributeChangeListener( final BeforeAttributeChangeListener< O > listener )
-	{
-		return beforeChangeListeners.remove( listener );
-	}
-
-	/**
-	 * Register a {@link AttributeChangeListener} that will be notified when the
-	 * value of this attribute has changed.
-	 *
-	 * @param listener
-	 *            the listener to register.
-	 * @return {@code true} if the listener was successfully registered.
-	 *         {@code false} if it was already registered.
-	 */
-	public boolean addAttributeChangeListener( final AttributeChangeListener< O > listener )
-	{
-		if ( !changeListeners.contains( listener ) )
-		{
-			changeListeners.add( listener );
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Removes the specified {@link AttributeChangeListener} from the set of
-	 * listeners.
-	 *
-	 * @param listener
-	 *            the listener to remove.
-	 * @return {@code true} if the listener was present in the listeners of this
-	 *         model and was successfully removed.
-	 */
-	public boolean removeAttributeChangeListener( final AttributeChangeListener< O > listener )
-	{
-		return changeListeners.remove( listener );
-	}
-
-	/**
-	 * Resume sending events to {@link BeforeAttributeChangeListener}s.
-	 */
-	public void pauseListeners()
-	{
-		emitEvents = false;
-	}
-
-	/**
-	 * Resume sending events to {@link BeforeAttributeChangeListener}s.
-	 */
-	public void resumeListeners()
-	{
-		emitEvents = true;
-	}
-
-	/**
 	 * Notify listeners that the value of this attribute for {@code object} is
 	 * about to change.
 	 *
@@ -130,9 +39,7 @@ public class Attribute< O >
 	 */
 	public void notifyBeforeAttributeChange( final O object )
 	{
-		if ( emitEvents )
-			for ( final BeforeAttributeChangeListener< O > l : beforeChangeListeners )
-				l.beforeAttributeChange( object );
+		super.notifyBeforePropertyChange( object );
 	}
 
 	/**
@@ -144,8 +51,6 @@ public class Attribute< O >
 	 */
 	public void notifyAttributeChanged( final O object )
 	{
-		if ( emitEvents )
-			for ( final AttributeChangeListener< O > l : changeListeners )
-				l.attributeChanged( object );
+		super.notifyPropertyChanged( object );
 	}
 }
