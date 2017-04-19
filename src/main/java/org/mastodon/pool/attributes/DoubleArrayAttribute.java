c74f4501599c07d8342fe106199bf90ef5d0acb0
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-04-19 18:01:35 +0200

    Add explicit notify() methods to DoubleArrayAttribute
    This is exposed publicly to be able to change a few elements with
    setQuiet() in a batch without sending notification for all of them.

--- src/main/java/org/mastodon/pool/attributes/DoubleArrayAttribute.java
+++ src/main/java/org/mastodon/pool/attributes/DoubleArrayAttribute.java
@@ -5,6 +5,8 @@ import static org.mastodon.pool.ByteUtils.DOUBLE_SIZE;
 import org.mastodon.pool.AbstractAttribute;
 import org.mastodon.pool.PoolObject;
 import org.mastodon.pool.PoolObjectLayout.DoubleArrayField;
+import org.mastodon.properties.BeforePropertyChangeListener;
+import org.mastodon.properties.PropertyChangeListener;
 
 public class DoubleArrayAttribute< O extends PoolObject< O, ?, ? > >
 	extends AbstractAttribute< O >
@@ -39,6 +41,36 @@ public class DoubleArrayAttribute< O extends PoolObject< O, ?, ? > >
 	public int length()
 	{
 		return length;
+	}
+
+	/**
+	 * Notify {@link BeforePropertyChangeListener}s that the value of this
+	 * property is about to change.
+	 * <p>
+	 * This is exposed publicly to be able to change a few elements with
+	 * {@link #setQuiet(PoolObject, int, double)} without sending notification
+	 * for all of them.
+	 * </p>
+	 */
+	@Override
+	public void notifyBeforePropertyChange( final O object )
+	{
+		super.notifyBeforePropertyChange( object );
+	}
+
+	/**
+	 * Notify {@link PropertyChangeListener}s that the value of this property
+	 * has changed.
+	 * <p>
+	 * This is exposed publicly to be able to change a few elements with
+	 * {@link #setQuiet(PoolObject, int, double)} without sending notification
+	 * for all of them.
+	 * </p>
+	 */
+	@Override
+	public void notifyPropertyChanged( final O object )
+	{
+		super.notifyPropertyChanged( object );
 	}
 
 	public DoubleArrayAttributeValue createAttributeValue( final O key )
