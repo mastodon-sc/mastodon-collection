68502c4db5146db20363fbd3d277e41de04875a4
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2017-06-10 20:18:23 +0200

    Distance to RealInterval and comparator based on this distance.

--- src/main/java/org/mastodon/rtree/GeometryUtil.java
+++ src/main/java/org/mastodon/rtree/GeometryUtil.java
@@ -1,6 +1,10 @@
 package org.mastodon.rtree;
 
+import java.util.Comparator;
+
 import net.imglib2.RealInterval;
+import net.imglib2.RealLocalizable;
+import net.imglib2.util.Intervals;
 
 /**
  * Static utilities related to geometrical computations for R-Trees.
@@ -39,21 +43,50 @@ public class GeometryUtil
 		return area;
 	}
 
-	public static final double distance( final RealInterval interval1, final RealInterval interval2 )
+	public static final double distanceSq( final RealInterval interval1, final RealInterval interval2 )
 	{
-		if (intersects(interval1, interval2))
+		if ( intersects( interval1, interval2 ) )
 			return 0.;
 
 		double d2 = 0.;
 		for ( int d = 0; d < interval1.numDimensions(); d++ )
 		{
-			final double dx = ( interval1.realMax( d ) < interval2.realMin( d ))
+			final double dx = ( interval1.realMax( d ) < interval2.realMin( d ) )
 					? interval2.realMin( d ) - interval1.realMax( d )
 							: interval1.realMin( d ) - interval2.realMax( d );
 
 					d2 += dx * dx;
 		}
-		return Math.sqrt( d2 );
+		return d2;
+	}
+
+	public static final double distance( final RealInterval interval1, final RealInterval interval2 )
+	{
+		return Math.sqrt( distanceSq( interval1, interval2 ) );
+	}
+
+	public static final double distanceSq( final RealInterval interval, final RealLocalizable point )
+	{
+		if ( Intervals.contains( interval, point ) )
+			return 0.;
+
+		double d2 = 0.;
+		for ( int d = 0; d < interval.numDimensions(); d++ )
+		{
+			double dx = point.getDoublePosition( d ) < interval.realMin( d )
+					? interval.realMin( d ) - point.getDoublePosition( d )
+							: ( point.getDoublePosition( d ) < interval.realMax( d )
+									? 0.
+											: point.getDoublePosition( d ) - interval.realMax( d ) );
+
+					d2 += dx * dx;
+		}
+		return d2;
+	}
+
+	public static final double distance( final RealInterval interval, final RealLocalizable point )
+	{
+		return Math.sqrt( distanceSq( interval, point ) );
 	}
 
 	public static final boolean intersects( final RealInterval interval1, final RealInterval interval2 )
@@ -93,6 +126,35 @@ public class GeometryUtil
 		out += ")";
 
 		return out;
+	}
+
+	public static final < K extends RealInterval > Comparator< K > distanceComparator( final RealLocalizable point )
+	{
+		return new DistComparator< K >( point );
+	}
+
+	/**
+	 * A comparator that sorts intervals with respect to how close they are to a
+	 * specified point.
+	 */
+	private static final class DistComparator< K extends RealInterval > implements Comparator< K >
+	{
+
+		private final RealLocalizable point;
+
+		public DistComparator( final RealLocalizable point )
+		{
+			this.point = point;
+		}
+
+		@Override
+		public int compare( final K o1, final K o2 )
+		{
+			double d1 = GeometryUtil.distanceSq( o1, point );
+			double d2 = GeometryUtil.distanceSq( o2, point );
+			return ( d1 < d2 ) ? -1
+					: ( d1 > d2 ) ? 1 : 0;
+		}
 	}
 
 	private GeometryUtil()
