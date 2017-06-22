f05dc4984c7e9f3ebd4907c140cd25542f36cb24
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-06-22 11:30:32 +0200

    Specialized wrappers for heterogeneous maps.
    We still want to expose the RefRefMap interface, and correctly
    deal with create*Ref() methods in the cases where we have only
    one of the two RefCollections that are pools.

---
+++ src/main/java/org/mastodon/collection/wrap/RefObjectMapWrapper.java
@@ -0,0 +1,132 @@
+package org.mastodon.collection.wrap;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.mastodon.collection.RefObjectMap;
+import org.mastodon.collection.RefRefMap;
+
+public class RefObjectMapWrapper< K, V > implements RefRefMap< K, V >
+{
+
+	private final RefObjectMap< K, V > map;
+
+	public RefObjectMapWrapper( final RefObjectMap< K, V > map )
+	{
+		this.map = map;
+	}
+
+	@Override
+	public boolean containsKey( final Object key )
+	{
+		return map.containsKey( key );
+	}
+
+	@Override
+	public boolean containsValue( final Object value )
+	{
+		return map.containsValue( value );
+	}
+
+	@Override
+	public Set< Entry< K, V >> entrySet()
+	{
+		return map.entrySet();
+	}
+
+	@Override
+	public V get( final Object key )
+	{
+		return map.get( key );
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		return map.isEmpty();
+	}
+
+	@Override
+	public Set< K > keySet()
+	{
+		return map.keySet();
+	}
+
+	@Override
+	public V put( final K key, final V value )
+	{
+		return map.put( key, value );
+	}
+
+	@Override
+	public void putAll( final Map< ? extends K, ? extends V > m )
+	{
+		map.putAll( m );
+	}
+
+	@Override
+	public V remove( final Object key )
+	{
+		return map.remove( key );
+	}
+
+	@Override
+	public int size()
+	{
+		return map.size();
+	}
+
+	@Override
+	public Collection< V > values()
+	{
+		return map.values();
+	}
+
+	@Override
+	public K createKeyRef()
+	{
+		return map.createKeyRef();
+	}
+
+	@Override
+	public void releaseKeyRef( final K obj )
+	{
+		map.releaseKeyRef( obj );
+	}
+
+	@Override
+	public void clear()
+	{
+		map.clear();
+	}
+
+	@Override
+	public V createValueRef()
+	{
+		return null;
+	}
+
+	@Override
+	public void releaseValueRef( final V obj )
+	{}
+
+	@Override
+	public V put( final K key, final V value, final V ref )
+	{
+		return put( key, value );
+	}
+
+	@Override
+	public V removeWithRef( final Object key, final V ref )
+	{
+		return remove( key );
+	}
+
+	@Override
+	public V get( final Object key, final V ref )
+	{
+		return get( key );
+	}
+
+}
