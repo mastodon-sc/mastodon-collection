a8abaa710d29c280dfe3d32688beff9c7e48b7cf
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-06-19 13:09:19 +0200

    Add javadoc

--- src/main/java/org/mastodon/undo/UndoPointMarker.java
+++ src/main/java/org/mastodon/undo/UndoPointMarker.java
@@ -1,7 +1,14 @@
 package org.mastodon.undo;
 
 /**
- * TODO javadoc
+ * Elements in a {@link UndoRedoStack} can be marked as being
+ * <em>undo-points</em>. Each high-level undo/redo step triggered by the user
+ * proceeds to undo/redo low-level elements until the next undo-point is
+ * reached.
+ * <p>
+ * Undo-points mark “stable states” in the order elements were recorded.
+ * Therefore, undo will stop immediately before undoing the undo-point, and redo
+ * will stop after redoing the undo-point.
  *
  * @author Tobias Pietzsch
  */
